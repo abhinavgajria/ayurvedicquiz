5e24a026566caaeb1fca7973278c5d2e91bcfc17
abhinav110101 <113776722+abhinav110101@users.noreply.github.com>
2023-09-16 01:09:11 +0530

    added more questions

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const questions = [
+    
     {
         question: "what are the main ingirdients in Amritarishta?",
         answeres: [
@@ -6,6 +7,15 @@ const questions = [
             { text: "	Arsha, Agnimandya, Udararoga, Vibandha", correct: false},
             { text: "Kandu, Tvak Vikara, Vibandha", correct: false},
             { text: "Balaroga, Balakshaya, Agnimandya, Aruchi", correct: false},
+        ]
+    },
+    {
+        question: "Which taste is associated with the Pitta dosha?",
+        answeres: [
+            { text: "Sweet", correct: false },
+            { text: "Bitter", correct: false },
+            { text: "Sour", correct: true },
+            { text: "Salty", correct: false }
         ]
     },
     {
@@ -27,12 +37,57 @@ const questions = [
         ]
     },
     {
+        question: "What is the Ayurvedic practice of self-massage with warm oil called?",
+        answeres: [
+            { text: "Acupuncture", correct: false },
+            { text: "Pranayama", correct: false },
+            { text: "Abhyanga", correct: true },
+            { text: "Meditation", correct: false }
+        ]
+    },
+    {
         question: "What are the main ingirdients in Aravindasava?",
         answeres: [
             { text: "SarvaJvara, Jirna Jvara", correct: false},
             { text: "	Arsha, Agnimandya, Udararoga, Vibandha", correct: false},
             { text: "Kandu, Tvak Vikara, Vibandha", correct: false},
             { text: "Balaroga, Balakshaya, Agnimandya, Aruchi", correct: true},
+        ]
+    },
+    {
+        question: "Which element is associated with the Vata dosha?",
+        answeres: [
+            { text: "Earth", correct: false },
+            { text: "Water", correct: false },
+            { text: "Air", correct: true },
+            { text: "Fire", correct: false }
+        ]
+    },
+    {
+        question: "What is the primary goal of Ayurvedic medicine?",
+        answeres: [
+            { text: "To cure diseases", correct: false },
+            { text: "To promote longevity and well-being", correct: true },
+            { text: "To perform surgery", correct: false },
+            { text: "To maintain body temperature", correct: false }
+        ]
+    },
+    {
+        question: "Which dosha is associated with the qualities of heaviness, coolness, and stability?",
+        answeres: [
+            { text: "Vata", correct: false },
+            { text: "Pitta", correct: false },
+            { text: "Kapha", correct: true },
+            { text: "Sattva", correct: false }
+        ]
+    },
+    {
+        question: "In Ayurveda, what is the term for the vital energy or life force within the body?",
+        answeres: [
+            { text: "Qi", correct: false },
+            { text: "Prana", correct: true },
+            { text: "Chakra", correct: false },
+            { text: "Yin", correct: false }
         ]
     },
     {
